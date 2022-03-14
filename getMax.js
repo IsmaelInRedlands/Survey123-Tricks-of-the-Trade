1519d6c6d24711098e1aa2df8625cde0b8bede65
Ismael Chivite <ichivite@esri.com>
2022-03-14 15:07:38 -0700

    Update getMax.js

--- getMax.js
+++ getMax.js
@@ -1,15 +1,17 @@
 function getMax(field,token){
    
+	//Replace with your own feature layer
 	var featureLayer = "https://services2.arcgis.com/fJJEXNgxjn0dpNsi/ArcGIS/rest/services/JTv2/FeatureServer/0";
 
 	var xmlhttp = new XMLHttpRequest();
 	var url = featureLayer + "/query?f=json&where=1=1&outStatistics=[{'statisticType':'max','onStatisticField':'" + field + "', 'outStatisticFieldName':'MaxValue'}]";
+        //Use next line, instead of above, if working with ArcGIS Enterprise
 	//var url = featureLayer + "/query?f=json&where=1=1& + encodeURI("outStatistics=[{'statisticType':'max','onStatisticField':'") + field + encodeURI("', 'outStatisticFieldName':'MaxValue'}]");
 
 
 	if (token){
         url = url + "&token=" + token;
-    }
+        }
 
 	xmlhttp.open("GET",url,false);
 	xmlhttp.send();
